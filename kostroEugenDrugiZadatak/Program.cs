6f7a719c7eae7e33b881e485b9e2462d7f5f2bd2
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-09-12 19:38:07 +0200

    Add files via upload

---
+++ kostroEugenDrugiZadatak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenPrviZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minuta, sekunda, brojSekundi;
+
+            Console.WriteLine("Upišite broj sekundi: ");
+            
+
+            minuta = sekunda / 60;
+            brojSekundi = sekunda % 60;
+
+            Console.WriteLine("minuta/e: ", minuta);
+            Console.WriteLine("sekundi/e: ", brojSekundi);
+
+            Console.ReadKey();
+        }
+    }
+}
